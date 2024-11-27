4639974e3c8ff1eb0c54c753f8218fc1d267a2a8
Timothy Thai <Jonanabanana1@gmail.com>
2024-11-26 21:03:54 -0600

    Added calculator bonuses

--- lesson_2/calculator.py
+++ lesson_2/calculator.py
@@ -1,44 +1,62 @@
+import json
+# Open the JSON file for reading
+with open('messages.json', 'r') as file:
+    data = json.load(file)
+
+
 def prompt(message):
     print(f'=> {message}')
 
 
 def invalid_number(number_str):
     try:
-        int(number_str)
+        float(number_str)
     except ValueError:
         return True
     return False
 
 
-prompt('Welcome to Calculator!')
-
-prompt("What's the first number?")
-number_1 = input()
-while invalid_number(number_1):
-    prompt('Error, please input a valid number')
+def calculate():
+    prompt(data[language]['ask_first_number'])
     number_1 = input()
+    while invalid_number(number_1):
+        prompt(data[language]['error_number'])
+        number_1 = input()
 
-prompt("What's the second number?")
-number_2 = input()
-while invalid_number(number_2):
-    prompt('Error, please input a valid number')
+    prompt(data[language]['ask_second_number'])
     number_2 = input()
+    while invalid_number(number_2):
+        prompt(data[language]['error_number'])
+        number_2 = input()
 
-prompt('What operation would you like to perform?\n'
-       '1: Addition  2: Subtraction  3: Multiplication  4: Division')
-operation = input()
-while operation not in ['1', '2', '3', '4']:
-    prompt('Error, please input a number between 1-4')
+    prompt(data[language]['ask_operation'])
     operation = input()
+    while operation not in ['1', '2', '3', '4']:
+        prompt(data[language]['error_number_list'])
+        operation = input()
 
-match operation:
-    case '1':
-        output = int(number_1) + int(number_2)
-    case '2':
-        output = int(number_1) - int(number_2)
-    case '3':
-        output = int(number_1) * int(number_2)
-    case '4':
-        output = int(number_1) / int(number_2)
+    match operation:
+        case '1':
+            output = float(number_1) + float(number_2)
+        case '2':
+            output = float(number_1) - float(number_2)
+        case '3':
+            output = float(number_1) * float(number_2)
+        case '4':
+            output = float(number_1) / float(number_2)
 
-print(f'The output is {output}')
+    prompt(f"{data[language]['output']} {output}")
+
+
+prompt('en: English, spa: Spanish')
+language = input()  # Could do more input validation here
+prompt(data[language]['welcome'])
+while True:
+    calculate()
+    prompt(data[language]['ask_retry'])
+    try_again = input()
+    while try_again not in ['y', 'n']:
+        prompt(data[language]['error_retry'])
+        try_again = input()
+    if try_again == 'n':
+        break
